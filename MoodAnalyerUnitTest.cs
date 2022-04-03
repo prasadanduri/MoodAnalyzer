6108d8845b15789c15ebb4fa7f2b613af9a0079d
prasad <prasadanduri13gmail.com>
2022-04-03 22:00:44 +0530

    Ability to analyze happy or sad

---
+++ MoodAnalyerUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProblem;
+
+namespace MoodAnalyzerUnitTest
+{
+    [TestClass]
+    public class MoodAnalyzerUnitTestCase
+    {
+
+        //given mood shoud return sad
+        public void GivenMood_ShouldReturn_Sad()
+        {
+            //expected string will be sad
+            string expected = "Sad";
+            //create a object of cass moodanalyzer with i am sad
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Sad");
+            //assertion is a boolean expression at a specific point in a program 
+            Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+        }
+        [TestMethod]
+        //given mood shoud return happy
+        public void GivenMood_ShouldReturn_Happy()
+        {
+            //expected string will be happy
+            string expected = "Happy";
+            //create a object of cass moodanalyzer with i am happy
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Happy");
+            //assertion is a boolean expression at a specific point in a program 
+            Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+        }
+
+        [TestMethod]
+        // Given Null Mood Should Return Happy
+        public void GivenNullMoodShouldReturnHappy()
+        {
+            //expected string will be happy
+            string expected = "Happy";
+            //create a object of cass moodanalyzer with Null value
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+            //assertion is a boolean expression at a specific point in a program 
+            Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+        }
+
+    }
+}
---
+++ MoodAnilizerPro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    // public classMood Analyzer
+    public class MoodAnalyzer
+    {//instance variable
+        private string Message;
+        //constructor for MoodAnalyzer 
+        public MoodAnalyzer(string message)
+        {
+            //this indicate difff of global name and method parrameter
+            this.Message = message;
+        }
+        //method for MoodAnalyzer
+        public string AnalyzeMood()
+        {
+            //UC2
+            try
+            {
+                // put the code here that may raise exceptions
+                //if message contains sad
+                if (this.Message.Contains("Sad"))
+                    // it will retrun sad
+                    return "Sad";
+                else
+                    //else happy
+                    return "Happy";
+            }
+            catch
+            {
+                // handle exception here
+                return "Happy";
+            }
+        }
+    }
+}
